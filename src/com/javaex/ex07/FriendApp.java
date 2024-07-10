e4d041e853b87c342fa4a87666b7ae1ab2748ef1
aripos1 <ihsjjang@gmail.com>
2024-07-10 17:19:49 +0900

    .

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -6,18 +6,22 @@ public class FriendApp {
 
 	public static void main(String[] args) {
 
-		Friend[] friendArray = new Friend[4];
+		Friend[] friendArray = new Friend[3];
 		Scanner sc = new Scanner(System.in);
 	
 		System.out.println("친구를 3명 등록해 주세요");
 		
+//		
+//		friendArray[0] = new Friend();
+//		friendArray[1] = new Friend();
+//		friendArray[2] = new Friend();
 	
 		// for문 시작
 		// 친구정보 3명 입력 로직 --> 반복문 사용
 		
 		
 		for (int i = 0; i < friendArray.length; i++) {
-			friendArray[i] = new Friend();		
+			friendArray[i] = new Friend();	// 친구 1(이름, 핸드폰, 학교) 친구 2 (이름, 핸드폰, 학교)친구 3 친구 4	
 			System.out.print("이름 : ");
 			friendArray[i].setName(sc.next());
 			System.out.print("핸드폰 : ");
